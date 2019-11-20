5724be61e9dd739b32b8a930ac173d05e27d45e7
Darnell23 <darnell.thomas@hotmail.co.uk>
2019-11-20 11:00:44 +0000

    setup node and express

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.static('views'));
 app.use(express.static('public'));
 app.use(express.urlencoded());
 app.engine('html',require('ejs').renderFile);
+
+const getApiData = formData =>
+{
+
+    return "nothing";
+}
+
 app.get('/',(req,res)=>{
 
     res.render('homepage.html')
@@ -13,7 +20,18 @@ app.get('/',(req,res)=>{
 
 app.post('/results/',(req,res)=>{
     console.log(req.body);
-    res.render('results.html', {data: req.body});
+    let apiData = getApiData(req.body);
+    console.log(apiData);
+    if(req.body.hasOwnProperty('btn_search'))
+    {
+        res.render('results.ejs', {data: req.body});
+        // console.log("Search");
+    }
+    else if (req.body.hasOwnProperty('btn_lucky'))
+    {
+        // console.log("Feeling Lucky");
+    }
+    
 })
 
 app.listen(port,()=> console.log(`listening on ${port}`))
